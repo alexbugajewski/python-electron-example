c0732d3a359c3c2247137cef5242a8fe8c6b4ab5
Alex Bugajewski <alexbug@iinet.net.au>
2021-08-05 21:03:03 +0800

    Working version using IPC via electron

--- src/electron/renderer.js
+++ src/electron/renderer.js
@@ -1,33 +1,26 @@
+const {ipcRenderer} = require('electron')
 const addButton = document.getElementById("add-button");
 const numOneField = document.getElementById("num-one");
 const numTwoField = document.getElementById("num-two");
 const resultField = document.getElementById("result");
-const url = "http://127.0.0.1:5000/";
 
 addButton.addEventListener("click", () => {
     let numOne = parseInt(numOneField.value);
     let numTwo = parseInt(numTwoField.value);
 
-    data = { numOne: numOne, numTwo: numTwo}
-    postData(data);
+    sendData(numOne, numTwo);
 })
 
-function postData(data = {}) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            let status = xhr.status;
-            if (status === 0 || (status >= 200 && status < 400)) {
-                let result = JSON.parse(xhr.responseText).result;
-                console.log(result);
-                resultField.innerHTML = result;
-            } else {
-                console.log("Error somehow...")
-            }
-        }
-    }
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
+function sendData(numOne, numTwo) {
+    console.log("sendData called!")
+    console.log(result.result);
     
-    xhr.send(JSON.stringify(data));
 }
+
+// Async message handler
+ipcRenderer.on('asynchronous-reply', (event, arg) => {
+   console.log(arg)
+})
+
+// Async message sender
+ipcRenderer.send('asynchronous-message', ['async ping'])
